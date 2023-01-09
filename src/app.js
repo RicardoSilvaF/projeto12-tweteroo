25fbbb2992bed88fee2e5728acff21b85b83b933
RicardoSilvaF <zanphs@gmail.com>
2023-01-09 07:18:05 -0300

    Implementacao dos posts/get signup e tweets

--- src/app.js
+++ src/app.js
@@ -7,31 +7,44 @@ server.use(cors());
 
 let newUser = {
 };
-let arrayTweets
+let arrayTweets = [];
 // SERVER SIGN UP 
 let users = [];
 
-server.get("/sign-up", (req,res) => {
-  
-})
-
 server.post("/sign-up", (req, res) => {
-  
+  newUser = req.body;
+  if(arrayTweets.length > 0){
+    if(arrayTweets[0].username != newUser.username){
+      arrayTweets = [];
+    }
+  }
+  res.send("OK")
 })
 // SERVER SIGN UP
 
 // SERVER TWEETS
 server.post("/tweets", (req, res) => {
-  
+  if(Object.keys(newUser).length === 0){
+    res.send("UNAUTHORIZED")
+  }
+  else{
+    let tweet = {
+      username: newUser.username,
+      avatar: newUser.avatar,
+      tweet: req.body.tweet
+    };
+    if(arrayTweets.length == 10){
+      arrayTweets.shift();
+    }
+    arrayTweets.push(tweet);
+    res.send("OK");
+  }
 })
 
 server.get("/tweets", (req,res) => {
-
+  res.send(arrayTweets);
 })
 // SERVER TWEETS
-
-
-
 
 server.listen(5000, () => {
 }) 
